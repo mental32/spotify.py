8a3cf41d2345743671487c06e2339ecdeabbfa62
mental32 <m3nta1@yahoo.com>
2018-05-04 07:50:06 +0100

    undo internal state changes

--- spotify/state.py
+++ spotify/state.py
@@ -53,15 +53,21 @@ class Cache:
         # other times the object may exist before the artist does
         # so when adding an artist check for anything that it should be aware of
 
-        if pool in ('_tracks', '_albums'):
+        if pool == '_tracks':
             # update other
             for parent in obj.artists:
                 if obj not in parent.tracks:
                     parent._cache[obj.id] = obj
 
-            if pool == '_tracks' and not obj.is_simple:
+            if not obj.is_simple:
                 if obj not in obj.album.tracks:
                     obj.album._cache[obj.id] = obj
+
+        elif pool == '_albums':
+            # update other
+            for parent in obj.artists:
+                if obj not in parent.albums:
+                    parent._cache[obj.id] = obj
 
         elif pool == '_artists':
             # update self
