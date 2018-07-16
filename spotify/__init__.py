7daa13aa9258871ada1ddd3c31a8383f818182f0
mental32 <m3nta1@yahoo.com>
2018-07-16 23:44:06 +0100

    updated _spotify_lookup logic

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -1,35 +1,15 @@
 ##
 # -*- coding: utf-8 -*-
 ##
-import functools
+from . import utils
 
-class _spotify__lookup(dict):
-
-	def artist(self, *args, **kwargs):
-		return self['artist'](*args, **kwargs)
-
-	def track(self, *args, **kwargs):
-		return self['track'](*args, **kwargs)
-
-	def user(self, *args, **kwargs):
-		return self['user'](*args, **kwargs)
-
-	def playlist(self, *args, **kwargs):
-		return self['playlist'](*args, **kwargs)
-
-	def album(self, *args, **kwargs):
-		return self['album'](*args, **kwargs)
-
-	def library(self, *args, **kwargs):
-		return self['library'](*args, **kwargs)
-
-
-_types = _spotify__lookup()
+_types = utils._spotify__lookup()
 
 from .errors import *
 from .models import *
 
-from .client import Client, _types
+from .client import Client
+from .http import HTTPClient
 
 __title__ = 'spotify'
 __author__ = 'mental'
--- spotify/utils.py
+++ spotify/utils.py
@@ -10,3 +10,18 @@ def ensure_http(func):
         return await func(self, *args, **kwargs)
 
     return decorator
+
+
+class _spotify__lookup(dict):
+    __types = ['artist', 'track', 'user', 'playlist', 'album', 'library']
+
+    def __getattribute__(self, key):
+        _types = object.__getattribute__(self, '_spotify__lookup__types')
+
+        if key in _types:
+            def _lookup(*args, **kwargs):
+                return self[key](*args, **kwargs)
+
+            return _lookup
+
+        return object.__getattribute__(self, key)
