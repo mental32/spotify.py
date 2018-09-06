02772d019de75f78e3c8ab157ddf7936fe6e43f3
mental <m3nta1@yahoo.com>
2018-09-06 10:57:25 +0100

    moved SynExecution thread start to sync/__init__.py

--- spotify/sync/__init__.py
+++ spotify/sync/__init__.py
@@ -4,3 +4,6 @@ from .sync import SyncExecution, _thread
 from .cls import (HTTPClient, HTTPUserClient, Client, Track, User, PlaylistTrack, Artist, Album, Playlist, Library)
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+_thread = SyncExecution()
+_thread.start()
