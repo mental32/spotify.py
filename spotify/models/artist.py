eb5f8d45cbcde5b9ede5e1193d9835234232ff9a
mental32 <m3nta1@yahoo.com>
2018-09-10 00:09:05 +0100

    spotify.Artist optimisations

--- spotify/models/artist.py
+++ spotify/models/artist.py
@@ -54,7 +54,7 @@ class Artist:
              The offset from where the api should start from in the albums.
         '''
         data = await self.__client.http.artist_albums(self.id, limit=limit, offset=offset, include_groups=include_groups, market=market)
-        return [Album(self.__client, item) for item in data['items']]
+        return list(Album(self.__client, item) for item in data['items'])
 
     async def get_all_albums(self, *, market='US'):
         '''loads all of the artists albums, depending on how many the artist has this may be a long operation.
@@ -83,7 +83,7 @@ class Artist:
         if market:
             kwargs['market'] = market
 
-        return (await self.__client.http.artist_albums(self.id, **kwargs)).get('total')
+        return (await self.__client.http.artist_albums(self.id, **kwargs))['total']
 
     async def top_tracks(self, country='US'):
         '''Get Spotify catalog information about an artist’s top tracks by country.
@@ -95,10 +95,10 @@ class Artist:
         '''
         data = (await self.__client.http.artist_top_tracks(self.id, country=country))['tracks']
 
-        return [Track(self.__client, item) for item in data]
+        return list(Track(self.__client, item) for item in data)
 
     async def related_artists(self):
         '''Get Spotify catalog information about artists similar to a given artist. Similarity is based on analysis of the Spotify community’s listening history.'''
         data = (await self.__client.http.artist_related_artists(self.id))['artists']
 
-        return [Artist(self.__client, item) for item in data]
+        return list(Artist(self.__client, item) for item in data)
