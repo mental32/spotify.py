51e28a9cf2d3e8cabafe3d693a2a99cb32b304b8
mental32 <m3nta1@yahoo.com>
2018-09-10 00:07:58 +0100

    changed import style

--- spotify/sync/__init__.py
+++ spotify/sync/__init__.py
@@ -5,6 +5,17 @@ from .sync import SyncExecution
 _thread = SyncExecution()
 _thread.start()
 
-from .cls import (HTTPClient, HTTPUserClient, Client, Track, User, PlaylistTrack, Artist, Album, Playlist, Library)
+from .cls import (
+	HTTPClient, 
+	HTTPUserClient, 
+	Client, 
+	Track, 
+	User, 
+	PlaylistTrack, 
+	Artist, 
+	Album, 
+	Playlist, 
+	Library
+)
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
