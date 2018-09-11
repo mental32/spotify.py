d3af672c7126057c763fc2350f894a7c6f2b57f6
mental32 <m3nta1@yahoo.com>
2018-09-11 23:18:52 +0100

    move SyncExecution Thread into `__init__`

--- spotify/sync/__init__.py
+++ spotify/sync/__init__.py
@@ -1,6 +1,46 @@
 import signal
+import asyncio
+import threading
+import queue
 
-from .sync import SyncExecution
+
+class SyncExecution(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._loop = asyncio.new_event_loop()
+        self.running = False
+        self.daemon = True
+
+        self.in_queue = queue.Queue(maxsize=1)
+        self.out_queue = queue.Queue(maxsize=1)
+
+    def run_coro(self, coro):
+        if self.running:
+            return coro
+
+        self.running = True
+        self.in_queue.put(coro)
+
+        while self.running:
+            if self.out_queue.full():
+                self.running = False
+                value = self.out_queue.get()
+                if isinstance(value, BaseException):
+                    raise value
+                return value
+
+    def run(self):
+        asyncio.set_event_loop(self._loop)
+
+        while True:
+            coro = self.in_queue.get()
+            try:
+                rv = self._loop.run_until_complete(coro)
+            except BaseException as error:
+                rv = error
+
+            self.out_queue.put(rv)
+        self._loop.close()
 
 _thread = SyncExecution()
 _thread.start()
