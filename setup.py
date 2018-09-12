de76a090797cd951ece0bb7366689fb83047f131
mental32 <m3nta1@yahoo.com>
2018-09-12 22:27:34 +0100

    remove pointless primitive version search

--- setup.py
+++ setup.py
@@ -13,17 +13,6 @@ def get_version():
       match = re.search(r"((\d\.){2,5}\d)", inf.read(), re.MULTILINE)
 
       if match is None:
-        # version regex broke
-        # use a primitive search method
-        inf.seek(0)
-        version_tag = '__version__ = '
-        version_tag_len = len(version_tag)
-
-        for line in inf.readlines():
-          if line[:version_tag_len] == version_tag:
-            return line[version_tag_len:].strip()
-
-      if match is None:
           raise RuntimeError('Version could not be found.')
 
       return match.groups()[0]
