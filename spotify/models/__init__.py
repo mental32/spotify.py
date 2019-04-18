c7a231199a176168360d9637ee09e3fea351e98e
mental <m3nta1@yahoo.com>
2019-04-19 00:03:26 +0100

    Update models `__init__`

--- spotify/models/__init__.py
+++ spotify/models/__init__.py
@@ -1,22 +1,31 @@
-__all__ = ('User', 'Track', 'PlaylistTrack', 'Artist', 'Album', 'Playlist', 'Library', 'Player', 'Device', 'Context', 'Image')
+from .. import _clean_namespace
+from .base import SpotifyBase, URIBase
+from . import typing
 
-from spotify import _types
+# To avoid circular/impossible imports.
+# We have to place "dead" refrences to
+# other models, such as Album, Track and User e.g
+# "Track: Optional[SpotifyBase] = None"
+# so that the modules can resolve the 
+# names correctly at compiletime then
+# by runtime the dead refrences will have 
+# been replaced with a refrence to the intended object.
 
-from .user import User
-from .track import Track, PlaylistTrack
-from .artist import Artist
-from .album import Album
-from .playlist import Playlist
-from .library import Library
-from .player import Player
 from .common import Device, Context, Image
+from .artist import Artist  # DEAD: Album Track
+from .track import Track, PlaylistTrack  # DEAD: Album
+from .player import Player
+from .album import Album
+from .library import Library
+from .playlist import Playlist, PartialTracks  # DEAD: User
+from .user import User
 
-_types.update({
-    'artist': Artist,
-    'track': Track,
-    'user': User,
-    'playlist': Playlist,
-    'album': Album,
-    'library': Library,
-    'playlist_track': PlaylistTrack
-})
+# swap the "dead" refrences
+from . import artist, playlist, track
+with _clean_namespace(locals(), 'track', 'artist', 'playlist'):
+    setattr(artist, 'Album', Album)
+    setattr(artist, 'Track', Track)
+    setattr(playlist, 'User', User)
+    setattr(track, 'Album', Album)
+
+__all__ = ('User', 'Track', 'PlaylistTrack', 'Artist', 'Album', 'Playlist', 'PartialTracks', 'Library', 'Player', 'Device', 'Context', 'Image')
