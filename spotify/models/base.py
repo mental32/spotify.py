5f21e27c26fa53054b068179ae6bcc88d7fe5cf9
mental <m3nta1@yahoo.com>
2019-04-22 22:54:19 +0100

    Add SpotifyBase.from_href

--- spotify/models/base.py
+++ spotify/models/base.py
@@ -12,6 +12,28 @@ class SpotifyBase:
 
         return object.__new__(cls)
 
+    async def from_href(self):
+        """Get the full object from spotify with a `href` attribute."""
+        if not hasattr(self, 'href'):
+            raise TypeError('Spotify object has no `href` attribute, therefore cannot be retrived')
+
+        elif hasattr(self, 'http'):
+            return await self.http.request(('GET', self.href))
+
+        else:
+            cls = type(self)
+
+        try:
+            client = getattr(self, '_{0}__client'.format(cls.__name__))
+        except AttributeError:
+            raise TypeError('Spotify object has no way to access a HTTPClient.')
+        else:
+            http = client.http
+
+        data = await http.request(('GET', self.href))
+
+        return cls(client, data)
+
 
 class URIBase(SpotifyBase):
     """Base class used for inheriting magic methods for models who have URIs."""
