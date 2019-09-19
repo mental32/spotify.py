e4057a3e3291dbf2c4692450ae3eff0dc664ec00
mental <m3nta1@yahoo.com>
2019-09-19 14:57:41 +0100

    Lint annotate and update utils

--- spotify/utils.py
+++ spotify/utils.py
@@ -1,70 +1,39 @@
-import inspect
-import functools
 import re
 from contextlib import contextmanager
-from urllib.parse import quote_plus as quote
-from typing import Callable
-
-from .errors import SpotifyException
+from typing import Iterable, Hashable
 
 _URI_RE = re.compile(r"^.*:([a-zA-Z0-9]+)$")
 _OPEN_RE = re.compile(r"http[s]?:\/\/open\.spotify\.com\/(.*)\/(.*)")
 
 
 @contextmanager
-def clean(l: dict, *names):
-    """A helper context manager that defers mutating a set of locals."""
+def clean(mapping: dict, *keys: Iterable[Hashable]):
+    """A helper context manager that defers mutating a mapping."""
     yield
-    for name in names:
-        l.pop(name)
+    for key in keys:
+        mapping.pop(key)
 
 
-def to_id(string: str) -> str:
+def to_id(value: str) -> str:
     """Get a spotify ID from a URI or open.spotify URL.
 
     Paramters
     ---------
-    string : str
-        The string to operate on.
+    value : :class:`str`
+        The value to operate on.
 
     Returns
     -------
-    id : str
-        The Spotify ID from the string.
+    id : :class:`str`
+        The Spotify ID from the value.
     """
-    string = string.strip()
-
-    match = _URI_RE.match(string)
+    value = value.strip()
+    match = _URI_RE.match(value)
 
     if match is None:
-        match = _OPEN_RE.match(string)
+        match = _OPEN_RE.match(value)
 
         if match is None:
-            return string
-        else:
-            return match.group(2)
-    else:
-        return match.group(1)
-
-
-def assert_hasattr(
-    attr: str, msg: str, tp: BaseException = SpotifyException
-) -> Callable:
-    """decorator to assert an object has an attribute when run."""
-
-    def decorator(func: Callable) -> Callable:
-        @functools.wraps(func)
-        def decorated(self, *args, **kwargs):
-            if not hasattr(self, attr):
-                raise tp(msg)
-            return func(self, *args, **kwargs)
-
-        if inspect.iscoroutinefunction(func):
-
-            @functools.wraps(func)
-            async def decorated(*args, **kwargs):
-                return await decorated(*args, **kwargs)
-
-        return decorated
-
-    return decorator
+            return value
+        return match.group(2)
+    return match.group(1)
