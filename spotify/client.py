e0bdce08c2478cc496da04fbdc7a089e289553ba
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-02-14 18:40:57 +0300

    feat(client): add get_multiple_shows method

--- spotify/client.py
+++ spotify/client.py
@@ -3,7 +3,7 @@ from typing import Optional, List, Iterable, NamedTuple, Type, Union, Dict
 
 from .http import HTTPClient
 from .utils import to_id
-from . import OAuth2, Artist, Album, Track, User, Playlist
+from . import OAuth2, Artist, Album, Track, User, Playlist, Show
 
 __all__ = ("Client", "SearchResults")
 
@@ -346,3 +346,19 @@ class Client:
                 for key, value in data.items()
             }
         )
+
+    async def get_multiple_shows(
+        self, ids: List[str], market: Optional[str] = "US"
+    ) -> List[Show]:
+        """Get Spotify catalog information for several shows based on their Spotify IDs.
+
+        Parameters
+        ----------
+        ids : List[:class:`str`]
+            A list of the Spotify IDs.
+        market : Optional[str]
+            An ISO 3166-1 alpha-2 country code.
+        """
+
+        data = await self.http.get_multiple_shows(ids, market)
+        return list(Show(self, show) for show in data["shows"])
