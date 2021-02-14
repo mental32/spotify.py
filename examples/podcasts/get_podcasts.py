70c5ecf55ec33d241a4c4f78414f6edd3f184f84
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-02-14 20:02:39 +0300

    chore: add examples

---
+++ examples/podcasts/get_podcasts.py
@@ -0,0 +1,25 @@
+import asyncio
+import spotify
+
+client = spotify.Client("some id", "some secret")
+
+
+async def main():
+
+    user = await client.user_from_token("token")
+
+    podcasts = await user.get_podcasts()
+
+    for podcast in podcasts:
+        print(
+            "Added at:",
+            podcast.added_at,
+            "Name:",
+            podcast.show.name,
+            "Description:",
+            podcast.show.description,
+        )
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
